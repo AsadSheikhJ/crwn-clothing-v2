90ce601ec9733f8ae40afe1da9cc0013fbfc514f
AsadSheikhJ <sheikhasad1652@gmail.com>
2023-07-12 11:39:11 +0500

    Sign In page create with firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -16,6 +16,9 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             LINK 1
           </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
