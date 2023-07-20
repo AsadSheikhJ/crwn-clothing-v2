41c285d1ac253869f644898554b8cb341c9f7313
AsadSheikhJ <sheikhasad1652@gmail.com>
2023-07-21 00:10:32 +0500

    redirect option

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 const SignIn = () => {
@@ -7,10 +8,15 @@ const SignIn = () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log(user);
+  };
   return (
     <div>
       <p>Sign In Page</p>
-      <button onClick={logGoogleUser}>CLICK ME TO SIGN IN</button>
+      <button onClick={logGoogleUser}>SIGN IN Google Popup</button>
+      <button onClick={logGoogleRedirectUser}>SIGN IN Goole Redirect</button>
     </div>
   );
 };
