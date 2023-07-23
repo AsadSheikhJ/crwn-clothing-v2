3111f2229d5f7e417cf48bcbe91a5bcea2b0bf52
AsadSheikhJ <sheikhasad1652@gmail.com>
2023-07-24 01:57:53 +0500

    some chnage in sign in page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,22 +1,29 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
   signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    console.log(user);
-  };
+
   return (
     <div>
       <p>Sign In Page</p>
       <button onClick={logGoogleUser}>SIGN IN Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>SIGN IN Goole Redirect</button>
+      <button onClick={signInWithGoogleRedirect}>SIGN IN Goole Redirect</button>
     </div>
   );
 };
