c82978444102b6e9fa5f00ea74f52e6de7fa0b54
AsadSheikhJ <sheikhasad1652@gmail.com>
2023-08-15 01:30:57 +0500

    user context

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -7,6 +7,8 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth
 } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../context/user.context";
 
 import './sign-up-form.styles.scss'
 
@@ -21,10 +23,14 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   }
-  console.log(formFields);
+
+
+  console.log('HIT');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -36,6 +42,8 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
+      setCurrentUser(user);
 
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
